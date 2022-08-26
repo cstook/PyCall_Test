fee7ad4f062979da76912385b56ace1f039f23a3
Christopher Stook <chris@chrisstook.com>
2022-08-26 12:09:24 -0400

    it works!

--- src/PyCall_Test.jl
+++ src/PyCall_Test.jl
@@ -2,7 +2,11 @@ module PyCall_Test
 export math, pysin
 
 using PyCall
-math = pyimport("math")
+
+const math = PyNULL()
+function __init__()
+    copy!(math, pyimport("math"))
+end
 pysin(x) = math.sin(x)
 
 end
